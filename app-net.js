beb70b0fefc2ea7fa69ecdf14e0050a50d026b91
Ryan Ostrager <ryan.ostrager@gmail.com>
2013-03-01 16:41:28 +0000

    Added some files that were accidently not included in the last commit.

--- app-net.js
+++ app-net.js
@@ -11,57 +11,58 @@ net.createServer(function(sock) {
 	sock.isAuthenticated=false;
 	users.push(sock);
 	sock.on('data', function(data) {
-                //check for end of stream value
-	if(data.toString().indexOf("<policy-file-request/>")!=-1)
-	{
-		sock.write('<?xml version="1.0"?><cross-domain-policy><allow-access-from domain="*" to-ports="3000-3010"/> </cross-domain-policy>');
-		sock.write('/n');
-		console.log("Sent crossdomain.xml");
-	}
-	if(data.length==1 && data[0]==0)
-	{
-		if(sock.data!=null)
+                console.log(data);
+		//check for end of stream value
+		if(data.toString().indexOf("<policy-file-request/>")!=-1)
 		{
-			data=concat(sock.data, data);
-			sock.data=null;
+			sock.write('<?xml version="1.0"?><cross-domain-policy><allow-access-from domain="*" to-ports="3000-3010"/> </cross-domain-policy>');
+			sock.write('/n');
+			console.log("Sent crossdomain.xml");
 		}
-		//slice off the end of transmission
-		data=data.slice(0, data.length-1);
-		//security settings for unity web player
-		$("Session.sessions").find({"_id" :parseSignedCookie(data.toString(), secret)}, function(r) {
-			if(r.documents[0]!=null)
+		if(data.length==1 && data[0]==0 && !sock.isAuthenticated)
+		{
+			if(sock.data!=null)
 			{
-				console.log("User was successfully authenticated");
-				sock.isAuthenticated=true;
+				data=concat(sock.data, data);
+				sock.data=null;
 			}
-			else//auth failed, close the connection
+			//slice off the end of transmission
+			data=data.slice(0, data.length-1);
+			//security settings for unity web player
+			$("Session.sessions").find({"_id" :parseSignedCookie(data.toString(), secret)}, function(r) {
+				if(r.documents[0]!=null)
+				{
+					console.log("User was successfully authenticated");
+					sock.isAuthenticated=true;
+				}
+				else//auth failed, close the connection
+				{
+					console.log("Invalid authentication - evil hacker detected");
+					sock.destroy()
+				}
+			});	
+		}
+		else if(data.length==1 && data[0]==1 && sock.isAuthenticated) //generic data handler for everything else
+		{
+			console.log("Something else");
+			//do something
+		}
+		else if(data.length==1 && data[0]=='2')//ping back the client
+		{
+			console.log("Got a ping");
+			sock.write("a");
+		}
+		else //stil in mid transition
+		{
+			if(sock.data!=null) //append the new data to already existing data
 			{
-				console.log("Invalid authentication - evil hacker detected");
-				sock.destroy()
+				sock.data=concat(sock.data,data);
 			}
-		});	
-	}
-	else if(data.length==1 && data[0]==1 && sock.isAuthenticated) //generic data handler for everything else
-	{
-		console.log("Something else");
-		//do something
-	}
-	else if(data.length==1 && data[0]==2)//ping back the client
-	{
-		console.log("Got a ping");
-		sock.write("a");
-	}
-	else //stil in mid transition
-	{
-		if(sock.data!=null) //append the new data to already existing data
-		{
-			sock.data=concat(sock.data,data);
+			else
+			{
+				sock.data=data;
+			}
 		}
-		else
-		{
-			sock.data=data;
-		}
-	}
 	});
 	sock.on('close', function(data) {
 		users.splice(users.indexOf(sock), 1);
@@ -79,6 +80,8 @@ concat = function (buf1, buf2)
 	tmp[1]=buf2;;
 	return Buffer.concat(tmp);
 }
+
+//taken from connect
 parseSignedCookie = function(str, secret){
   return 0 == str.indexOf('s:')
     ? unsign(str.slice(2), secret)
